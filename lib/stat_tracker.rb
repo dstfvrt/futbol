7289d7402983fa4891bc9f3f81d299212f45363e
Dustin Favorite <dstfvrt@gmail.com>
2019-09-12 08:48:28 -0500

    Add fixtures, game record class

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -2,13 +2,16 @@ require "csv"
 require "pry"
 class StatTracker
   def initialize
+    @games = Hash.new
+    @teams = Hash.new
+    @games_teams = Hash.new
   end
 
   def self.from_csv(locations)
-    locations.each do |location|
-      CSV.foreach(location[1]) do |row|
-        puts row
-      end
-    end
+    games_csv = CSV.open locations[:games], headers: true, header_converters: :symbol
+    games_teams_csv = CSV.open locations[:game_teams], headers: true, header_converters: :symbol
+    teams_csv = CSV.open locations[:teams], headers: true, header_converters: :symbol
+
+    binding.pry
   end
 end
