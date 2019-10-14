6258fcfbd7f1c62248eef6c327bc1348f99d8839
Dustin Favorite <dstfvrt@gmail.com>
2019-10-14 14:35:13 -0500

    Slight tweaks to records property Now .csv is only evaluated @ initialize

--- lib/repository.rb
+++ lib/repository.rb
@@ -1,14 +1,16 @@
 require "csv"
 
 class Repository
-  attr_reader :filepath, :record_class
+  attr_reader :filepath, :record_class, :records
   def initialize(filepath, record_class)
     @filepath = filepath
     @record_class = record_class
+    @records = []
+    make_records
   end
 
-  def records
-    CSV.open(filepath, headers: true, header_converters: :symbol)
+  def make_records
+    records = CSV.open(filepath, headers: true, header_converters: :symbol)
       .map do |record|
       record_class.new(record)
     end
