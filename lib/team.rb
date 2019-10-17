82b1489fd8f304ad886aeed9dcf73305bb90ced7
Dustin Favorite <dstfvrt@gmail.com>
2019-10-17 14:56:04 -0500

    WIP

--- lib/team.rb
+++ lib/team.rb
@@ -22,6 +22,7 @@ class Team
     home_scores(games).reduce(:+) / home_scores(games).size
   end
 
+<<<<<<< Updated upstream
   def away_scores(games)
     games.select { |game| game.away_team_id == team_id }
       .map { |game| game.away_goals }
@@ -31,15 +32,28 @@ class Team
     game_teams.select do |game_team|
       games.map { |game| game.game_id unless game.home_team_id != team_id }
         .contains(game_team.game_id)
+=======
+  def away_games(games)
+    games.select { |game| game.away_team_id == team_id }
+  end
+
+  def blocked_home_shots(games, game_teams)
+    games.select { |game| game.home_team_id == team_id }
+>>>>>>> Stashed changes
   end
 
   def franchise_id
     @attributes[:franchiseId].to_i
   end
 
+<<<<<<< Updated upstream
   def home_scores(games)
     games.select { |game| game.home_team_id == team_id }
       .map { |game| game.home_goals }
+=======
+  def home_games(games)
+    games.select { |game| game.home_team_id == team_id }
+>>>>>>> Stashed changes
   end
 
   def link
