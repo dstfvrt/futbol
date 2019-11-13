a14742d7e4ed29ccebc912cfb7a582f9a683f798
Dustin Favorite <dstfvrt@gmail.com>
2019-11-13 16:03:26 -0600

    Finish porting Team methods to TeamGameStats In addition, memoized TeamGameStats.games to assure all games are a single team's games

--- lib/team.rb
+++ lib/team.rb
@@ -5,20 +5,6 @@ class Team < Record
     attributes[:abbreviation]
   end
 
-  def all_goals_scored
-    games.map do |game|
-      if game.home_team_id == id
-        game.home_goals
-      else
-        game.away_goals
-      end
-    end
-  end
-
-  def average_home_score
-    home_games.sum(&:home_goals) / (home_games.size.nonzero? || 1)
-  end
-
   def average_score
     games.sum do |game|
       if game.home_team_id == id
@@ -29,18 +15,8 @@ class Team < Record
     end / (games.size.nonzero? || 1)
   end
 
-  def average_visiting_score
-    away_games.sum(&:away_goals) / (away_games.size.nonzero? || 1)
-  end
-
   def away_games
     games.select { |game| game.away_team_id == id }
-  end
-
-  def away_record
-    (away_games.count do |game|
-      game.winner?(self)
-    end / (away_games.size.nonzero? || 1).to_f).to_f
   end
 
   def franchise_id
@@ -49,16 +25,6 @@ class Team < Record
 
   def games
     @games ||= build_games
-  end
-
-  def home_games
-    games.select { |game| game.home_team_id == id }
-  end
-
-  def home_record
-    (home_games.count do |game|
-      game.winner?(self)
-    end / (home_games.size.nonzero? || 1).to_f).to_f
   end
 
   def link
@@ -76,49 +42,7 @@ class Team < Record
   def name
     @attributes[:teamname]
   end
-
-  def number_of_wins
-    games.count { |game| game.winner?(self) }
-  end
-
-  def number_lost_by_season
-    seasons = games.map(&:season).uniq
-
-    seasons.each_with_object({}) do |season, hash|
-      season_games = games.select do |game|
-        game.season == season
-      end
-
-      hash[season] = season_games.count do |game|
-        game.losing_team_id == id
-      end
-    end
-  end
-
-  def number_of_wins_by_season
-    seasons = games.map(&:season).uniq
-
-    seasons.each_with_object({}) do |season, hash|
-      season_games = games.select do |game|
-        game.season == season
-      end
-
-      hash[season] = season_games.count do |game|
-        game.winning_team_id == id
-      end
-    end
-  end
-
-  def opponents
-    games.map do |game|
-      if game.home_team_id == id
-        game.away_team_id
-      else
-        game.home_team_id
-      end
-    end.uniq!
-  end
-
+  
   private
 
   def build_games
--- spec/team_game_stats_spec.rb
+++ spec/team_game_stats_spec.rb
@@ -4,9 +4,11 @@ require "./lib/team_game_stats"
 RSpec.describe TeamGameStats do
   describe "#initialize" do
     it "takes in a team, and the team's games" do
-      team = double("team")
-      games = [double("game"), double("game")]
-
+      team = double("Team", id: 1)
+      games = [
+        double("Game", home_team_id: 1, away_team_id: 0),
+        double("Game", home_team_id: 1, away_team_id: 0),
+      ]
       game_stats = TeamGameStats.new(team: team, games: games)
 
       expect(game_stats.team).to eq team
@@ -21,7 +23,6 @@ RSpec.describe TeamGameStats do
         double("Game", home_team_id: team.id, away_goals: 3, away_team_id: 0),
         double("Game", home_team_id: 0, home_goals: 7, away_team_id: team.id),
       ]
-
       game_stats = build_game_stats(team: team, games: games)
 
       expect(game_stats.average_allowed_goals).to eq 5
@@ -29,20 +30,22 @@ RSpec.describe TeamGameStats do
   end
 
   describe "#average_home_score" do
-    xit "returns the average score of all home games" do
+    it "returns the average score of all home games" do
+      team = instance_double(Team, id: 1)
       games = [
-        double("Game", home_team_id: 1, home_goals: 3),
-        double("Game", home_team_id: 0, home_goals: 4),
-        double("Game", home_team_id: 1, home_goals: 5),
+        double("Game", home_team_id: 1, home_goals: 3, away_team_id: 0),
+        double("Game", home_team_id: 0, home_goals: 4, away_team_id: 0),
+        double("Game", home_team_id: 1, home_goals: 5, away_team_id: 0),
       ]
-      allow(team).to receive(:games).and_return(games)
+      game_stats = build_game_stats(team: team, games: games)
 
       expect(game_stats.average_home_score).to eq 4
     end
   end
 
   describe "#average_score" do
-    xit "returns the average score across all games" do
+    it "returns the average score across all games" do
+      team = instance_double(Team, id: 1)
       games = [
         double("Game", home_team_id: 1, home_goals: 3, away_team_id: 0,
                        away_goals: 0),
@@ -51,14 +54,15 @@ RSpec.describe TeamGameStats do
         double("Game", home_team_id: 1, home_goals: 5, away_team_id: 0,
                        away_goals: 0),
       ]
-      allow(team).to receive(:games).and_return(games)
+      game_stats = build_game_stats(team: team, games: games)
 
       expect(game_stats.average_score).to eq 4
     end
   end
 
   describe "#average_visiting_score" do
-    xit "returns the average score across all away games" do
+    it "returns the average score across all away games" do
+      team = instance_double(Team, id: 1)
       games = [
         double("Game", home_team_id: 0, home_goals: 0, away_team_id: 1,
                        away_goals: 2),
@@ -67,20 +71,21 @@ RSpec.describe TeamGameStats do
         double("Game", home_team_id: 1, home_goals: 5, away_team_id: 0,
                        away_goals: 0),
       ]
-      allow(team).to receive(:games).and_return(games)
+      game_stats = build_game_stats(team: team, games: games)
 
       expect(game_stats.average_visiting_score).to eq 3
     end
   end
 
   describe "#away_games" do
-    xit "returns an array of all games where the team is a visitor" do
+    it "returns an array of all games where the team is a visitor" do
+      team = instance_double(Team, id: 1)
       games = [
         double("Game", home_team_id: 0, away_team_id: 1),
         double("Game", home_team_id: 0, away_team_id: 1),
         double("Game", home_team_id: 1, away_team_id: 0),
       ]
-      allow(team).to receive(:games).and_return(games)
+      game_stats = build_game_stats(team: team, games: games)
 
       expect(game_stats.away_games).to eq (games
         .select { |game| game.away_team_id == team.id })
@@ -88,27 +93,29 @@ RSpec.describe TeamGameStats do
   end
 
   describe "#away_record" do
-    xit "returns a decimal percentage of the teams visiting win ratio" do
+    it "returns a decimal percentage of the teams visiting win ratio" do
+      team = instance_double(Team, id: 1)
       games = [
         double("Game", home_team_id: 0, away_team_id: 1, winner?: true),
         double("Game", home_team_id: 0, away_team_id: 1, winner?: false),
         double("Game", home_team_id: 1, away_team_id: 0, winner?: true),
         double("Game", home_team_id: 1, away_team_id: 0, winner?: false),
       ]
-      allow(team).to receive(:games).and_return(games)
+      game_stats = build_game_stats(team: team, games: games)
 
       expect(game_stats.away_record).to eq 0.5
     end
   end
 
   describe "#games" do
-    xit "returns an array of all games the team participated in" do
+    it "returns an array of all games the team participated in" do
+      team = instance_double(Team, id: 1)
       games = [
         double("Game", home_team_id: 1, away_team_id: 0),
         double("Game", home_team_id: 0, away_team_id: 1),
         double("Game", home_team_id: 0, away_team_id: 0),
       ]
-      allow(team.database).to receive(:games).and_return(games)
+      game_stats = build_game_stats(team: team, games: games)
 
       expect(game_stats.games).to eq (games
         .select do |game|
@@ -118,13 +125,14 @@ RSpec.describe TeamGameStats do
   end
 
   describe "#home_games" do
-    xit "returns an array of all games where the team is home" do
+    it "returns an array of all games where the team is home" do
+      team = instance_double(Team, id: 1)
       games = [
         double("Game", home_team_id: 1, away_team_id: 0),
         double("Game", home_team_id: 0, away_team_id: 1),
         double("Game", home_team_id: 0, away_team_id: 0),
       ]
-      allow(team.database).to receive(:games).and_return(games)
+      game_stats = build_game_stats(team: team, games: games)
 
       expect(game_stats.home_games).to eq (games
         .select { |game| game.home_team_id == team.id })
@@ -132,118 +140,115 @@ RSpec.describe TeamGameStats do
   end
 
   describe "#home_record" do
-    xit "returns a decimal percentage of the teams home win ratio" do
+    it "returns a decimal percentage of the teams home win ratio" do
+      team = instance_double(Team, id: 1)
       games = [
         double("Game", home_team_id: 0, away_team_id: 1, winner?: true),
         double("Game", home_team_id: 0, away_team_id: 1, winner?: false),
         double("Game", home_team_id: 1, away_team_id: 0, winner?: true),
         double("Game", home_team_id: 1, away_team_id: 0, winner?: false),
       ]
-      allow(team).to receive(:games).and_return(games)
+      game_stats = build_game_stats(team: team, games: games)
 
       expect(game_stats.home_record).to eq 0.5
     end
   end
 
   describe "#number_of_wins" do
-    xit "returns the total number of games the team has won" do
+    it "returns the total number of games the team has won" do
+      team = instance_double(Team, id: 1)
       games = [
         double("Game", home_team_id: 0, away_team_id: 1, winner?: true),
         double("Game", home_team_id: 0, away_team_id: 1, winner?: false),
         double("Game", home_team_id: 1, away_team_id: 0, winner?: true),
         double("Game", home_team_id: 1, away_team_id: 0, winner?: false),
       ]
-      allow(team).to receive(:games).and_return(games)
+      game_stats = build_game_stats(team: team, games: games)
 
       expect(game_stats.number_of_wins).to eq 2
     end
   end
 
   describe "#number_of_wins_by_season" do
-    xit "returns a hash of seasons with their related win counts" do
-      team = build_team
+    it "returns a hash of seasons with their related win counts" do
+      team = instance_double(Team, id: 1)
       games = [
-        instance_double(Game, season: 20122013, winning_team_id: 1),
-        instance_double(Game, season: 20122013, winning_team_id: 1),
-        instance_double(Game, season: 20132014, winning_team_id: 1),
-        instance_double(Game, season: 20132014, winning_team_id: 2),
+        double("Game", season: 20122013, winning_team_id: 1, home_team_id: 1),
+        double("Game", season: 20122013, winning_team_id: 1, home_team_id: 1),
+        double("Game", season: 20132014, winning_team_id: 1, home_team_id: 1),
+        double("Game", season: 20132014, winning_team_id: 2, home_team_id: 1),
       ]
-      allow(team)
-        .to receive(:games)
-        .and_return(games)
+      game_stats = build_game_stats(team: team, games: games)
 
       team_hash = {
         20122013 => 2,
         20132014 => 1,
       }
+
       expect(game_stats.number_of_wins_by_season).to eq team_hash
     end
   end
 
   describe "#number_lost_by_season" do
-    xit "returns a hash of seasons with their related losing counts" do
-      team = build_team
+    it "returns a hash of seasons with their related losing counts" do
+      team = instance_double(Team, id: 1)
       games = [
-        instance_double(Game, season: 20122013, losing_team_id: 1),
-        instance_double(Game, season: 20122013, losing_team_id: 1),
-        instance_double(Game, season: 20132014, losing_team_id: 1),
-        instance_double(Game, season: 20132014, losing_team_id: 2),
+        double("Game", season: 20122013, losing_team_id: 1, home_team_id: 1),
+        double("Game", season: 20122013, losing_team_id: 1, home_team_id: 1),
+        double("Game", season: 20132014, losing_team_id: 1, home_team_id: 1),
+        double("Game", season: 20132014, losing_team_id: 2, home_team_id: 1),
       ]
-      allow(team)
-        .to receive(:games)
-        .and_return(games)
+      game_stats = build_game_stats(team: team, games: games)
 
       team_hash = {
         20122013 => 2,
         20132014 => 1,
       }
+
       expect(game_stats.number_lost_by_season).to eq team_hash
     end
   end
 
   describe "#all_goals_scored" do
-    xit "returns an array of all the goals a team has scored" do
-      team = build_team
+    it "returns an array of all the goals a team has scored" do
+      team = instance_double(Team, id: 1)
       games = [
-        instance_double(Game, home_team_id: 1, home_goals: 1),
-        instance_double(Game, home_team_id: 1, home_goals: 3),
-        instance_double(Game, home_team_id: 2, away_team_id: 1, away_goals: 2),
-        instance_double(Game, home_team_id: 2, away_team_id: 2, away_goals: 4),
+        double("Game", home_team_id: 1, home_goals: 1),
+        double("Game", home_team_id: 1, home_goals: 3),
+        double("Game", home_team_id: 2, away_team_id: 1, away_goals: 2),
+        double("Game", home_team_id: 2, away_team_id: 2, away_goals: 4),
       ]
-      allow(team)
-        .to receive(:games)
-        .and_return(games)
+      game_stats = build_game_stats(team: team, games: games)
 
-      goals_array = [1, 3, 2, 4]
+      goals_array = [1, 3, 2]
+
       expect(game_stats.all_goals_scored).to eq goals_array
     end
   end
 
   describe "#oppenents" do
-    xit "returns an array of opponents of a team" do
-      team = build_team
+    it "returns an array of opponents of a team" do
+      team = instance_double(Team, id: 1)
       opponent_array = [2, 3]
       games = [
-        instance_double(Game, {
+        double("Game", {
           home_team_id: 1,
           away_team_id: 2,
         }),
-        instance_double(Game, {
+        double("Game", {
           home_team_id: 1,
           away_team_id: 2,
         }),
-        instance_double(Game, {
+        double("Game", {
           home_team_id: 1,
           away_team_id: 3,
         }),
-        instance_double(Game, {
+        double("Game", {
           home_team_id: 3,
           away_team_id: 1,
         }),
       ]
-      allow(team)
-        .to receive(:games)
-        .and_return(games)
+      game_stats = build_game_stats(team: team, games: games)
 
       expect(game_stats.opponents).to eq opponent_array
     end
